b100d558b6b282dcbb0e46ae049f0b13dfd91db3
Thiago <1theeldergod@gmail.com>
2026-05-19 20:11:39 -0300

    Estrutura Base da Carteira

---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        Carteira minhaCarteira = new Carteira("Thiago");
+
+        minhaCarteira.depositar(0.5);
+
+        System.out.println(
+            "Saldo de Bitcoin: "
+            + minhaCarteira.saldoBitcoin
+        );
+    }
+}
